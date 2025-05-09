6d549c30d6da438aa5b312f8cd58f1f893980a2f
mthedoodler <108304734+mthedoodler@users.noreply.github.com>
2025-05-10 00:19:00 +0100

    Started displaying quests as well as changing their status depending on state.

---
+++ tests.js
@@ -0,0 +1,15 @@
+ let firstQuest = new Quest("Hello World", 0, 0, "", true);
+
+let secondQuest = new Quest("First Child", 150, 50);
+
+let thirdQuest = new Quest("Second Child", 150, -50);
+
+firstQuest.addDependent(secondQuest);
+firstQuest.addDependent(thirdQuest);
+
+console.assert(secondQuest.prerequisiteList(), "Hello World"); // Hello World
+console.assert(thirdQuest.prerequisiteList(), "Hello World"); // Hello World
+console.assert(firstQuest.dependentsList(), "First Child, Second Child"); // First Child, Second Child
+
+
+
